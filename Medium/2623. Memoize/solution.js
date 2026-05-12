ed67b0903d6ae35b668bf7b92a85887b86ddfa5f
mubirshami <mubirshami2000@gmail.com>
2026-05-13 02:13:42 +0500

    feat: add README and solution for 2623

---
+++ Medium/2623. Memoize/solution.js
@@ -0,0 +1,12 @@
+function memoize(fn) {
+    let cache = {};
+    return function(...args) {
+        let key = JSON.stringify(args);
+        if (key in cache) {
+            return cache[key];
+        }
+        let result = fn(...args);
+        cache[key] = result;
+        return result;
+    };
+}
